22637138dd304a8255b1b710b92e9fcd45c9f5f6
仁志 <legend1world@gmail.com>
2021-01-18 07:51:37 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,24 @@
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+var CACHE_NAME = 'pwa-caches';
+var urlsToCache = [
+    '/linkevery2s.github.io/bsinsight/',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
 });
-
-self.addEventListener('fetch', function(event) {});
