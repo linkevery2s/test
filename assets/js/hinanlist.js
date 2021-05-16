0863870cd752165903ca14522febe07f0f9e77a4
仁志 <legend1world@gmail.com>
2021-05-16 16:42:42 +0900

    up

--- assets/js/hinanlist.js
+++
@@ -1,7 +0,0 @@
-function list(){
-
-	var num = document.getElementById("number").value;
-	
-	document.getElementById("hinanlist").innerHTML = '<iframe width="100%" height="700" scrolling="yes" frameborder="no" src="hinanlist/' + num + '.html"></iframe>';
-
-}
